cdaae90a2ad5f951c5cd3adae7032dc889f306c8
Bea <beatrice@springtimesoft.co.nz>
2024-02-03 14:27:43 +1300

    last commit

--- obj/Debug/net8.0/WebAssembly.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAssembly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ade7d5a45db6c8859fd380602e771f228c5518")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
